287163bfe9b31abdf92ed7acc2771b8706a0d592
mylenosa <mylenavnunes2004@gmail.com>
2025-07-08 14:29:35 -0400

    Prepara projeto para deploy

--- jogo_da_forca/settings.py
+++ jogo_da_forca/settings.py
@@ -28,9 +28,9 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mylenosa.pythonanywhere.com']
 
 
 # Application definition
