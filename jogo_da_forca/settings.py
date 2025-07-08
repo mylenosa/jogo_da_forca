63752bcdb51fb0514f2102b5065485ae3ac46ca3
mylenosa <mylenavnunes2004@gmail.com>
2025-07-08 14:40:21 -0400

    Prepara projeto para deploy

--- jogo_da_forca/settings.py
+++ jogo_da_forca/settings.py
@@ -10,22 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Adicione esta linha para carregar o ficheiro .env
 load_dotenv(os.path.join(BASE_DIR, '.env'))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
